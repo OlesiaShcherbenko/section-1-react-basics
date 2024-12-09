2e6a093b8b49e7f418d6ac699d9a48eef1fd77f6
Olesia Shcherbenko <oshch96@gmail.com>
2024-12-09 13:49:01 +0100

    Lesson 7: JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
+const reactElement = <h1>Hello from JSX!</h1>
+
+console.log(reactElement)
+
 root.render(
-  createElement("h1", null, "Hello from createElement!")
+  reactElement
 )
